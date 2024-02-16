2ce1348b0c4968a6d3b563a36a147d605bb25520
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-16 16:11:21 +0200

    generating counter selector file

--- src/app/counter-controls/counter-controls.component.ts
+++ src/app/counter-controls/counter-controls.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { DecrementAction, IncrementAction, ResetAction } from '../store/counter.actions';
-// import { decrement, increment, reset } from '../store/counter.actions';
+import { 
+  // DecrementAction, IncrementAction, ResetAction,
+   decrement, increment, reset } from '../store/counter.actions';
 
 @Component({
   selector: 'app-counter-controls',
@@ -13,15 +14,19 @@ import { DecrementAction, IncrementAction, ResetAction } from '../store/counter.
 export class CounterControlsComponent {
   constructor(private store: Store) {}
 increment(){
-  // this.store.dispatch(increment({value:1}))
+  this.store.dispatch(increment({value:1}))
 
-  this.store.dispatch(new IncrementAction(1))
+  // this.store.dispatch(new IncrementAction(1))
 }
 decrement(){
-  this.store.dispatch(new DecrementAction(2))
+  this.store.dispatch(decrement({value:2}))
+
+  // this.store.dispatch(new DecrementAction(2))
 }
 reset(){
-  this.store.dispatch(new ResetAction(0))
+  this.store.dispatch(reset())
+
+  // this.store.dispatch(new ResetAction(0))
 }
   
 }
--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -1,7 +1,7 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import {
-  DecrementAction,
-  IncrementAction,
+  // DecrementAction,
+  // IncrementAction,
   decrement,
   increment,
   reset,
@@ -9,24 +9,24 @@ import {
 // import { decrement, increment, reset } from '../store/counter.actions';
 
 const initialState = 0;
-// export const countReducer = createReducer(
-//   initialState,
-//   on(increment, (state,action) => state + action.value),
-//   on(decrement, (state,action) => state - action.value),
-//   on(reset, (state) => state = 0)
-// );
+export const countReducer = createReducer(
+  initialState,
+  on(increment, (state,action) => state + action.value),
+  on(decrement, (state,action) => state - action.value),
+  on(reset, (state) => state = 0)
+);
 
 // or I can use another way:
-export function countReducer(
-  state = initialState,
-  action: IncrementAction | Action
-) {
-  if (action.type === increment) {
-    return state + (<IncrementAction>action).value;
-  } else if (action.type === decrement) {
-    return state - (action as DecrementAction).value;
-  } else if (action.type === reset) {
-    return (state = 0);
-  }
-  return state;
-}
+// export function countReducer(
+//   state = initialState,
+//   action: IncrementAction | Action
+// ) {
+//   if (action.type === increment) {
+//     return state + (<IncrementAction>action).value;
+//   } else if (action.type === decrement) {
+//     return state - (action as DecrementAction).value;
+//   } else if (action.type === reset) {
+//     return (state = 0);
+//   }
+//   return state;
+// }
