e9a0644c2e4fc545a39a384174f80c79125813a2
Maha Fathy <143645938+mahamfathy@users.noreply.github.com>
2024-02-16 13:07:26 +0200

    adding counter reducer function without creating the reducer

--- src/app/store/counter.reducer.ts
+++ src/app/store/counter.reducer.ts
@@ -2,13 +2,24 @@ import { createReducer, on } from '@ngrx/store';
 import { decrement, increment, reset } from './counter.actions';
 
 const initialState = 0;
-export const countReducer = createReducer(
-  initialState,
-  on(increment, (state,action) => state + action.value),
-  on(decrement, (state,action) => state - action.value),
-  on(reset, (state) => state = 0)
-);
-//or I can use another way:
-// export function countReducer(state = initialState){
-//     return state
-// }
+// export const countReducer = createReducer(
+//   initialState,
+//   on(increment, (state,action) => state + action.value),
+//   on(decrement, (state,action) => state - action.value),
+//   on(reset, (state) => state = 0)
+// );
+
+// or I can use another way:
+export function countReducer(state = initialState, action: any) {
+  if (action.type === '[Counter] Increment') {
+    return state + action.value;
+  }else  if (action.type === '[Counter] Decrement') {
+    return state - action.value;
+    
+  } else if (action.type === '[Counter] Reset') {
+    return state = 0 ;
+    
+  } 
+  return state;
+    
+}
